a795ad98de525a5838537f8406a0a57988028da8
smisann <pc.smithfield@gmail.com>
2025-11-30 13:50:40 +0900

    ファイル分けをした

--- attiny10-asm-templates-01/AsmFile1.asm
+++
@@ -1,6 +0,0 @@
-/*
- * AsmFile1.asm
- *
- *  Created: 2025/11/30 13:31:41
- *   Author: pcsmi
- */ 
---
+++ led-blink/main.asm
@@ -0,0 +1,12 @@
+;
+; led-blink.asm
+;
+; Created: 2025/11/30 13:48:43
+; Author : pcsmi
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
